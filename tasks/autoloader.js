43691ee932efaab5f4cfe831e4a7815404eff3ae
Simon Mongeau <simon@imagerie.ca>
2018-12-09 16:07:34 -0500

    Update autoloader.js

--- tasks/autoloader.js
+++ tasks/autoloader.js
@@ -22,7 +22,7 @@ include('vendor/autoload.php');
 Balise\\AnchorFramework\\Anchor::Init();
 
 // LOAD EVERYTHING IN content
-$files=glob(__DIR__."/app/content/*.php");
+$files=glob(__DIR__."/app/content/*/*.php");
 foreach ($files as $file) {
     require_once($file);
 }
