3baaed2bc521b053cfb14f7f941637ab98dd4bb8
Simon Mongeau <simon@imagerie.ca>
2019-01-23 10:38:20 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,41 @@ var HubRegistry = require('gulp-hub');
 /* tell gulp to use the tasks just loaded */
 gulp.registry(new HubRegistry(['tasks/*.js']));
 
-const toWatch = [
-    'images',
-    'admin-style',
-    'public-style'
-];
-
 // START
-gulp.task('watch', () => {
+
+gulp.task('watcher', () => {
+    // Graphic tasks
     gulp.watch('app/graphics/icons/*.svg', gulp.series(gulp.parallel('iconfont'), gulp.parallel('public-style'), 'autoloader'));
-    gulp.watch('app/graphics/dashicons/*.svg', gulp.series(gulp.parallel('dashicons'), gulp.parallel('public-style'), 'autoloader'));
+    gulp.watch('app/graphics/dashicons/*.svg', gulp.series(gulp.parallel('dashicons'), gulp.parallel('admin-style'), 'autoloader'));
     gulp.watch('app/graphics/images/*', gulp.series(gulp.parallel('images'), gulp.parallel('public-style'), 'autoloader'));
 
-    gulp.watch('app/**/*.js*', gulp.series(gulp.parallel('js'), 'autoloader'));
-     gulp.watch('components/**/*public.js*', gulp.series(gulp.parallel('js'), 'autoloader'));
-     gulp.watch('components/**/block.js*', gulp.series(gulp.parallel('components'), 'autoloader'));
-     gulp.watch('components/**/*.*ss', gulp.series(gulp.parallel('public-style'), 'autoloader'));
-     gulp.watch('components/**/*.*ss', gulp.series(gulp.parallel('admin-style'), 'autoloader'));
-     gulp.watch('app/**/*.*', gulp.parallel(...toWatch));
+    // Public Elements
+    gulp.watch('app/**/*.js*', gulp.series(gulp.parallel('public-script'), 'autoloader'));
+    gulp.watch('app/**/*.ss*', gulp.series(gulp.parallel('public-style'), 'autoloader'));
+    
+    // Gutenberg Elements
+    gulp.watch('components/**/*public.js*', gulp.series(gulp.parallel('public-script'), 'autoloader'));
+    gulp.watch('components/**/*.js*', gulp.series(gulp.parallel('gutenberg'), 'autoloader'));
+    gulp.watch('components/**/*.*ss', gulp.series(gulp.parallel('public-style'), 'autoloader'));
+    gulp.watch('components/**/*.*ss', gulp.series(gulp.parallel('admin-style'), 'autoloader'));
+    
+    // Autoloader for PHP
     gulp.watch(['app/**/*.php'], gulp.parallel('autoloader'));
 });
-gulp.task('default', gulp.series('iconfont', gulp.parallel(...toWatch), 'autoloader'));
+gulp.task('watch', gulp.series('default', 'watcher'));
+
+
+gulp.task('default', gulp.series(
+    gulp.parallel([
+        'iconfont',
+        'dashicons'
+    ]),
+    gulp.parallel([
+        'images',
+        'admin-style',
+        'public-style',
+        'admin-script',
+        'public-script'
+    ]),
+    'autoloader'
+));
