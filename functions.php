4ea24946d689c1ad04b24993eb406ab41f0aad98
Simon Mongeau <simon@imagerie.ca>
2019-01-23 19:57:36 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,4 @@
 <?php
 // LOAD COMPOSER AUTO LOADER
 include('vendor/autoload.php');
-
-// LOAD EVERYTHING IN content
-$files=glob(__DIR__."/app/content/*.php");
-foreach ($files as $file) {
-    require_once($file);
-}
+Balise\AnchorFramework\Anchor::Init();
