71fc01983039ef4a9f11d194f02d3c5de9629041
Simon Mongeau <simon@imagerie.ca>
2020-11-23 16:04:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Ceci est un commentaire
-
-use Balise\AnchorFramework\Anchor as BaliseAnchor;
-BaliseAnchor::render();
+// Un comment the following line if you wish to use Blade
+//
+// use Balise\AnchorFramework\Anchor as BaliseAnchor;
+// BaliseAnchor::render();
